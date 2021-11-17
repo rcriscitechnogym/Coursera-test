0d5558d90e04c8c9116afeb763a6ddf5c3653fa1
Crisci Riccardo - Technogym SPA <rcrisci@technogym.com>
2021-11-17 15:49:21 +0100

    update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -21,6 +21,12 @@ var facebook = {
 
 /* function sono object, puoi assegnarli proprietà!*/
 
+
+/*si usa for stile python per scorrere le proprietà*/
+for (var prop in facebook) {
+    console.log(prop);    
+}
+
 function multiply(x,y) {
     return x+y;
 }
@@ -39,3 +45,24 @@ function makeMultiplier(multiplier){
 
 var raddoppia= makeMultiplier(2);
 console.log(raddoppia(25));
+
+/* Function constructors*/
+
+function Circle(radius){
+    console.log(this); 
+}
+
+var myCircle = new Circle(10);   /* con il new punta alla funzione perciò il console log mi restituisce l'object Circle, ricorda che function sono object!*/
+console.log(myCircle);
+
+
+/*array*/
+
+var array = new Array();
+array[0]="Riccardo";
+array[1]= function (name){
+    console.log("Ciao" + name)
+};
+
+/*per richiamare funzione nell'array*/
+array[1](array[0]); /* dico che voglio invocare funzione passando cone name= array[0] */
