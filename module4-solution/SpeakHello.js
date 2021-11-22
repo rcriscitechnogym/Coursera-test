4abd46d5f8a8570e6977ab0f46b7a1184e2a12ad
Crisci Riccardo - Technogym SPA <rcrisci@technogym.com>
2021-11-22 10:40:30 +0100

    ass#4

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,19 +1,10 @@
-// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-// See Lecture 52, part 2
-
-(function (window){
-
-    var helloSpeaker = {};
+(function (window) {
     var speakWord = "Hello";
-
-// STEP 4: Rewrite the 'speak' function such that it is attached to the
-// helloSpeaker object instead of being a standalone function.
-// See Lecture 52, part 2
-    helloSpeaker.speak= function (name) {
+    var helloSpeaker = {
+      speak: function (name) {
         console.log(speakWord + " " + name);
+      }
     };
-window.helloSpeaker = helloSpeaker;
-})(window);
-// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-// 'helloSpeaker' on the global scope as well.
-// See Lecture 52, part 2
+    window.helloSpeaker = helloSpeaker;
+  })(window);
+  
