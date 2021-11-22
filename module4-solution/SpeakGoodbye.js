a3687e2d2c25bfad7855285b5ae20707ce3b3500
Crisci Riccardo - Technogym SPA <rcrisci@technogym.com>
2021-11-22 09:57:33 +0100

    ass#4

--- module4-solution/SpeakGoodbye.js
+++ module4-solution/SpeakGoodbye.js
@@ -8,7 +8,7 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var byeSpeaker = new Object();
+var byeSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -16,8 +16,8 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.speak=function (name) {
-  console.log(speakWord + " " + name);
+byeSpeaker.speak=function (names) {
+  console.log(speakWord + " " + names);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
