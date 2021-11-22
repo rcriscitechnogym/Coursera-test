b1e7381766543bbe5cb3c2b97649a5fa2b60fb8a
Crisci Riccardo - Technogym SPA <rcrisci@technogym.com>
2021-11-22 10:01:53 +0100

    ass#4

--- module4-solution/SpeakGoodbye.js
+++ module4-solution/SpeakGoodbye.js
@@ -18,9 +18,10 @@ var speakWord = "Good Bye";
 // See Lecture 52, part 2
 byeSpeaker.speak=function (names) {
   console.log(speakWord + " " + names);
-}
+
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 window.byeSpeaker = byeSpeaker;
+}
 })(window);
